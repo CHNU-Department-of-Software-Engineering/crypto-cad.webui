f44f36fdd97f10c36968281ba4fd64aa5051124a
mykolaKushniryk <mykola.kushniryk@gmail.com>
2020-02-02 19:36:17 +0200

    Initial commit

---
+++ src/CryptoCAD.API/Controllers/DESController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoCAD.API.Controllers
+{
+    [ApiController]
+    [Route("api/des")]
+    public class DESController : ControllerBase
+    {
+        private readonly ILogger<DESController> _logger;
+        public DESController(ILogger<DESController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Стандарт шифрования данных DES (DATA ENCRYPTION STANDARD) – блочный шифр с симметричными ключами, разработан Национальным Институтом Стандартов и Технологии (NIST – National Institute of Standards and Technology).");
+        }
+    }
+}
